b0233935d37918263f4bd372f0ac0d5415b0b9a5
Klaus Heino <klaus.heino@gmail.com>
2017-10-14 15:04:59 +0300

    Make the size of a circle smaller

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var dy = -2;
 
 function createBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 5, 0, Math.PI*2);
+  ctx.arc(x, y, 3, 0, Math.PI*2);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
