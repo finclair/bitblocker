f43c2d2262427cbcd9cc55423c42a0a5002bf46b
Klaus Heino <klaus.heino@gmail.com>
2017-10-14 04:03:00 +0300

    Create functionality which adds animation for drawn circle clearRect() clears everything drawn on canvas and variables x and y are different each time when draw function is executed.

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ setInterval(draw, 10);
 
 var x = canvas.width/2;
 var y = canvas.height -30;
+var dx = 2;
+var dy = -2;
 
-function draw () {
+
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
   ctx.beginPath();
   ctx.arc(x, y, 5, 0, Math.PI*2);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
+  x = x + dx;
+  y = y + dy;
 }
 
