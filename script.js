ea26913d53b0912f107012a2968444a73ce51313
Klaus Heino <klaus.heino@gmail.com>
2017-10-14 03:49:57 +0300

    Add a function which draws a circle on the canvas

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 var canvas = document.getElementById('canvasBoard');
 var ctx = canvas.getContext('2d');
+
+setInterval(draw, 10);
+
+var x = canvas.width/2;
+var y = canvas.height -30;
+
+function draw () {
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, Math.PI*2);
+  ctx.fillStyle = "green";
+  ctx.fill();
+  ctx.closePath();
+}
+
